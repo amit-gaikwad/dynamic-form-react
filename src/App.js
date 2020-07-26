1064e1993c76cff3b25c566d9b232db657238111
Amit Gaikwad <amitgaikwad795@gmail.com>
2020-07-26 15:19:43 +0530

    add react setup and install antd/prettier husky

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      This is MentorLink
+      <Button type="primary">This is Antd</Button>
+    </div>
+  );
+}
+
+export default App;
