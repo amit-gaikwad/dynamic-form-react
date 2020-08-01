80abb00b6d35f5122c0dc37847ed5d157056489a
Amit Gaikwad <amitgaikwad795@gmail.com>
2020-08-01 14:22:43 +0530

    fix edit flow

--- src/Components/UserDetails/UserDetails.jsx
+++ src/Components/UserDetails/UserDetails.jsx
@@ -29,7 +29,7 @@ const UserDetails = (props) => {
     debugger;
     const newResource = omit(templateResource, ['resourceId']);
     const istemplateResource = newResource.attributes.find(
-      (attr) => attr.attribute.keyName !== 'template'
+      (attr) => attr.attribute.keyName == 'template'
     );
     newResource.attributes = newResource.attributes.filter(
       (attr) => attr.attribute.keyName !== 'template'
@@ -92,15 +92,96 @@ const UserDetails = (props) => {
           }
           const currentIndexAttr =
             attributes.find((a) => a.attribute.keyName == 'currentIndex') || {};
+          const fields = getFieldsFromAttributeModels(attributes);
+          const isItTemplate = fields.find((f) => f.label === 'template');
+          const isItHavingMultiResource = fields.find((f) => f.label === 'Instances Allowed');
+          const currentResource = userResource || template;
           return (
             <Row style={{ margin: '10px' }} key={template.resourceId}>
-              <CollapsedDetails
+              <Row style={{ width: '100%' }}>
+                <Row style={{ width: '100%' }}>
+                  <Col span={22} style={{ fontWeight: 1000 }}>
+                    {currentResource.resourceName}
+                  </Col>
+                  <Col span={2}>
+                    <Button
+                      onClick={() => {
+                        if (!isItTemplate && isItHavingMultiResource) {
+                          addNewResourceClick(currentResource);
+                        } else {
+                          onEditClick(currentResource);
+                        }
+                      }}>
+                      {/* {isItTemplate ? '+' : 'Edit'} */}
+                      {!isItTemplate ? (
+                        isItHavingMultiResource && (
+                          <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 24 24'
+                            data-supported-dps='24x24'
+                            fill='currentColor'
+                            width='24'
+                            height='24'
+                            focusable='false'>
+                            <path d='M21 13h-8v8h-2v-8H3v-2h8V3h2v8h8v2z'></path>
+                          </svg>
+                        )
+                      ) : (
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          viewBox='0 0 24 24'
+                          data-supported-dps='24x24'
+                          fill='currentColor'
+                          width='24'
+                          height='24'
+                          focusable='false'>
+                          <path d='M21 13h-8v8h-2v-8H3v-2h8V3h2v8h8v2z'></path>
+                        </svg>
+                      )}
+                    </Button>
+                  </Col>
+                </Row>
+                {!isItTemplate && (
+                  <Row style={{ width: '100%', marginTop: '15px' }} className='multiResources'>
+                    <Col span={6}>
+                      {fields.map((field) => {
+                        if (!['template', 'userId', 'currentIndex'].includes(field.label)) {
+                          return (
+                            <Col key={field.label} span={24}>
+                              {field.label} : {field.value}
+                            </Col>
+                          );
+                        }
+                      })}
+                    </Col>
+                    <Col span={2} className='editButton'>
+                      <Button
+                        onClick={() => {
+                          onEditClick(currentResource);
+                        }}>
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          viewBox='0 0 24 24'
+                          data-supported-dps='24x24'
+                          fill='currentColor'
+                          width='24'
+                          height='24'
+                          focusable='false'>
+                          <path d='M21.71 5L19 2.29a1 1 0 00-.71-.29 1 1 0 00-.7.29L4 15.85 2 22l6.15-2L21.71 6.45a1 1 0 00.29-.74 1 1 0 00-.29-.71zM6.87 18.64l-1.5-1.5L15.92 6.57l1.5 1.5zM18.09 7.41l-1.5-1.5 1.67-1.67 1.5 1.5z'></path>
+                        </svg>
+                      </Button>
+                    </Col>
+                  </Row>
+                )}
+              </Row>
+
+              {/* <CollapsedDetails
                 addNewResourceClick={addNewResourceClick}
                 fields={getFieldsFromAttributeModels(attributes)}
                 template={userResource || template}
                 currentIndex={currentIndexAttr.keyValue || 0}
                 onEditClick={onEditClick}
-              />
+              /> */}
               <Divider />
             </Row>
           );
